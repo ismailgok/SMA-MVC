7586faef53ba60b0dde07c2fff5f9c01f15b4251
İsmail <ismayilgok16@gmail.com>
2021-12-21 23:54:41 +0300

    new

---
+++ ecommorce1.21.12.21/ecommorce1.21.12.21/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ecommorce1._21._12._21.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sepet()
+        {
+            return View();
+        }
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Siparis()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ ecommorce1.21.12.21/ecommorce1.21.12.21/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ecommorce1._21._12._21.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
